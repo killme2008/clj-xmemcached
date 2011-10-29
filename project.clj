c2eaa6bf8e35ae3d5d4a997c7a4772aa76e61407
dennis zhuang <killme2008@gmail.com>
2011-10-29 22:27:29 +0800

    Add lein-clojars dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-xmemcached "0.1.0-SNAPSHOT"
+(defproject clj-xmemcached "0.1.0"
   :author "dennis zhuang[killme2008@gmail.com]"
   :description "An opensource memcached client for clojure wrapping xmemcached"
   :dependencies [
@@ -6,4 +6,5 @@
 				 [com.googlecode.xmemcached/xmemcached "1.3.5"]]
   :dev-dependencies [
 					 [log4j/log4j "1.2.16"]
-					 [org.slf4j/slf4j-log4j12 "1.5.6"]])
+					 [org.slf4j/slf4j-log4j12 "1.5.6"]
+					 [lein-clojars "0.6.0"]])
