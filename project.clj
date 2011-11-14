495514ac2026a1c98487ba7de0dba4def236ff04
dennis zhuang <killme2008@gmail.com>
2011-11-14 16:52:51 +0800

    Upgrade version to 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject clj-xmemcached "0.1.1"
+(defproject clj-xmemcached "0.1.2-SNAPSHOT"
   :author "dennis zhuang[killme2008@gmail.com]"
   :description "An opensource memcached client for clojure wrapping xmemcached"
   :dependencies [
 				 [org.clojure/clojure "1.2.1"]
-				 [com.googlecode.xmemcached/xmemcached "1.3.5"]]
+				 [com.googlecode.xmemcached/xmemcached "1.3.6-SNAPSHOT"]]
   :dev-dependencies [
 					 [log4j/log4j "1.2.16"]
 					 [org.slf4j/slf4j-log4j12 "1.5.6"]
