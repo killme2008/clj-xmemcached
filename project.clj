f5a030c2674c4e41488ce8d5895c65fe9b0ddb3d
dennis zhuang <killme2008@gmail.com>
2015-02-12 14:07:29 +0800

    Released 0.2.6-RC1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-xmemcached "0.2.6-SNAPSHOT"
+(defproject clj-xmemcached "0.2.6-RC1"
   :author "dennis zhuang[killme2008@gmail.com]"
   :description "An opensource memcached client for clojure wrapping xmemcached"
   :dependencies [[org.clojure/data.json "0.2.4"]
