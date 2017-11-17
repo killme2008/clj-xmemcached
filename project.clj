a3bbf41326afa56dce552dfcd0524e26d4f03c67
dennis zhuang <killme2008@gmail.com>
2017-11-17 10:10:10 +0800

    (feat) Bump xmemcached and data.json version, close #5

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject clj-xmemcached "0.4.0"
   :author "dennis zhuang[killme2008@gmail.com]"
   :description "An opensource memcached client for clojure wrapping xmemcached"
-  :dependencies [[org.clojure/data.json "0.2.4"]
+  :dependencies [[org.clojure/data.json "0.2.6"]
                  [org.slf4j/slf4j-log4j12 "1.5.6"]
                  [com.taoensso/nippy "2.6.3"]
                  [org.clojure/clojure "1.8.0"]
-                 [com.googlecode.xmemcached/xmemcached "2.2.0"]]
+                 [com.googlecode.xmemcached/xmemcached "2.4.0"]]
   :test-paths ["test" "example"]
   :profiles {:dev {:dependencies [[log4j/log4j "1.2.16"]
                                   [org.slf4j/slf4j-log4j12 "1.5.6"]]
